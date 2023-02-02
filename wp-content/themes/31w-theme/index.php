401f8900f33c5554d9efa174180e4d9686a6c880
Antoine Boutin <aboutin140@icloud.com>
2023-02-02 15:09:16 -0500

    Ajout d'un header et d'un footer. Réglage d'un problème avec le thème qui demandait une version minimale de WordPress qui n'existe pas.

--- wp-content/themes/31w-theme/index.php
+++ wp-content/themes/31w-theme/index.php
@@ -1,1 +1,15 @@
-<?php
+<?php get_header(); ?>
+
+<main>
+    <h3>index.php</h3>
+
+    <?php
+        while (have_posts())
+        {
+            the_post();
+            the_title("<h1>", "</h1>");
+        }
+    ?>
+</main>
+
+<?php get_footer(); ?>
