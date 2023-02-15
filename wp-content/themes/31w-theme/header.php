18f502746d33248309076881c75132b483ee7702
Antoine Boutin <aboutin140@icloud.com>
2023-02-15 09:59:15 -0500

    Début de la structure du menu en SASS

--- wp-content/themes/31w-theme/header.php
+++ wp-content/themes/31w-theme/header.php
@@ -12,6 +12,11 @@
         $blogInfoUrl = get_bloginfo("url");
         $blogInfoUrl = filter_var($blogInfoUrl, FILTER_SANITIZE_URL);
         $blogInfoUrl = filter_SSL($blogInfoUrl);
+
+        wp_nav_menu([
+           "menu" => "entete",
+            "container" => "nav"
+        ]);
     ?>
     <h1><a href="<?php echo($blogInfoUrl); ?>"><?php echo(filter_var(get_bloginfo("name"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
     <h2><?php echo(filter_var(get_bloginfo("description"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></h2>
