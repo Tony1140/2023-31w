74ea53ad6cbbb1f412edfa1071db893d2d17e74e
Antoine Boutin <aboutin140@icloud.com>
2023-02-15 10:19:06 -0500

    Menu SASS

--- wp-content/themes/31w-theme/header.php
+++ wp-content/themes/31w-theme/header.php
@@ -9,14 +9,13 @@
 <body>
 <header>
     <?php
-        $blogInfoUrl = get_bloginfo("url");
-        $blogInfoUrl = filter_var($blogInfoUrl, FILTER_SANITIZE_URL);
-        $blogInfoUrl = filter_SSL($blogInfoUrl);
-
         wp_nav_menu([
            "menu" => "entete",
             "container" => "nav"
         ]);
+
+        $blogInfoUrl = get_bloginfo("url");
+        $blogInfoUrl = filter_var($blogInfoUrl, FILTER_SANITIZE_URL);
     ?>
     <h1><a href="<?php echo($blogInfoUrl); ?>"><?php echo(filter_var(get_bloginfo("name"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
     <h2><?php echo(filter_var(get_bloginfo("description"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></h2>
--- wp-content/themes/31w-theme/index.php
+++ wp-content/themes/31w-theme/index.php
@@ -9,7 +9,7 @@
             the_post();
         }
     ?>
-    <h1><a href="<?php echo(filter_SSL(filter_var(get_the_permalink(), FILTER_SANITIZE_URL))); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
+    <h1><a href="<?php echo(filter_var(get_the_permalink(), FILTER_SANITIZE_URL)); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
     <?php echo(filter_var(wp_trim_words(get_the_excerpt(), 4), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?>
 </main>
 
