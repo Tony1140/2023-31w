df0670da5d1e6eff8d6ba31ff45bcf31e0f7faeb
Antoine Boutin <aboutin140@icloud.com>
2023-02-25 13:03:25 -0500

    Premier de category.php

--- wp-content/themes/category.php
+++ wp-content/themes/category.php
@@ -10,7 +10,7 @@
             echo($fileName);
         ?>
     </code>
-    <section class="recherche">
+    <section class="blocflex">
         <?php
             while (have_posts())
             {
@@ -18,9 +18,8 @@
         ?>
         <article>
             <h5><a href="<?php echo(filter_var(get_the_permalink(), FILTER_SANITIZE_URL)); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h5>
-            <?php echo(filter_var(wp_trim_words(get_the_excerpt(), 30), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?>
+            <?php echo(filter_var(wp_trim_words(get_the_excerpt(), 4), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?>
         </article>
-        <hr>
         <?php } ?>
     </section>
 </main>
