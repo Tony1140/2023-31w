93f117b591bbb75f12295cb134f72cd574dae7dd
Antoine Boutin <aboutin140@icloud.com>
2023-02-25 12:45:03 -0500

    Modifier page d'accueil

--- wp-content/themes/31w-theme/front-page.php
+++ wp-content/themes/31w-theme/front-page.php
@@ -11,14 +11,19 @@
         ?>
     </code>
     <h3>index.php</h3>
-    <?php
-        while (have_posts())
-        {
-            the_post();
-        }
-    ?>
-    <h1><a href="<?php echo(filter_var(get_the_permalink(), FILTER_SANITIZE_URL)); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
-    <?php echo(filter_var(wp_trim_words(get_the_excerpt(), 4), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?>
+    <section class="blocflex">
+        <?php
+            while (have_posts())
+            {
+                the_post();
+
+        ?>
+        <article>
+            <h3><a href="<?php echo(filter_var(get_the_permalink(), FILTER_SANITIZE_URL)); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h3>
+            <p><?php echo(filter_var(wp_trim_words(get_the_excerpt(), 4), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></p>
+        </article>
+        <?php } ?>
+    </section>
 </main>
 
 <?php get_footer(); ?>
