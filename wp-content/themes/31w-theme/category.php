194d0fe2004b90077166dd9c1533310e5bfd5e1d
Antoine Boutin <aboutin140@icloud.com>
2023-02-25 13:11:44 -0500

    WP_Query dans category.php

--- wp-content/themes/31w-theme/category.php
+++ wp-content/themes/31w-theme/category.php
@@ -1,26 +1,24 @@
 <?php get_header(); ?>
 
-<main>
-    <code>
-        <?php
-            $fileName = $_SERVER["SCRIPT_FILENAME"];
-            $fileName = filter_var($fileName, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $fileName = basename($fileName, ".php");
-
-            echo($fileName);
-        ?>
-    </code>
+<main class="site__main">
     <section class="blocflex">
         <?php
-            while (have_posts())
+            $category = get_queried_object();
+            $args = [
+                "category_name" => $category->slug,
+                "orderby" => "title",
+                "order" => "ASC"
+            ];
+            $query = new WP_Query($args);
+            while ($query->have_posts())
             {
-                the_post();
+                $query->the_post();
         ?>
         <article>
             <h5><a href="<?php echo(filter_var(get_the_permalink(), FILTER_SANITIZE_URL)); ?>"><?php echo(filter_var(get_the_title(), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h5>
             <?php echo(filter_var(wp_trim_words(get_the_excerpt(), 4), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?>
         </article>
-        <?php } ?>
+        <?php } wp_reset_postdata(); ?>
     </section>
 </main>
 
--- wp-content/themes/31w-theme/header.php
+++ wp-content/themes/31w-theme/header.php
@@ -9,18 +9,20 @@
 <body>
 <header class="site__entete">
     <section class="logomenu">
-         <?php
-            the_custom_logo();
-            wp_nav_menu([
-               "menu" => "entete",
-                "container" => "nav"
-            ]);
+        <?php the_custom_logo(); ?>
+        <div class="menusearch">
+            <?php
+                wp_nav_menu([
+                   "menu" => "entete",
+                    "container" => "nav"
+                ]);
 
-            get_search_form();
+                get_search_form();
 
-            $blogInfoUrl = get_bloginfo("url");
-            $blogInfoUrl = filter_var($blogInfoUrl, FILTER_SANITIZE_URL);
-        ?>
+                $blogInfoUrl = get_bloginfo("url");
+                $blogInfoUrl = filter_var($blogInfoUrl, FILTER_SANITIZE_URL);
+            ?>
+        </div>
     </section>
     <h1><a href="<?php echo($blogInfoUrl); ?>"><?php echo(filter_var(get_bloginfo("name"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></a></h1>
     <h2><?php echo(filter_var(get_bloginfo("description"), FILTER_SANITIZE_FULL_SPECIAL_CHARS)); ?></h2>
